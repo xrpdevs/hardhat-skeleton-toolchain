0544ff0c195877118c7e73a4b9e1830687a35392
Jamie Prince <78679039+xrpdevs@users.noreply.github.com>
2022-03-22 10:11:05 +0000

    Update mainnet.js

--- configs/mainnet.js
+++ configs/mainnet.js
@@ -11,6 +11,7 @@ require("@ericxstone/hardhat-blockscout-verify");
 
 require("hardhat/config");
 
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
